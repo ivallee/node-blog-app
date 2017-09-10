fe7dd3fe762825ed5c9172183b6f596789ab79d1
Isaac Vallee <isaacvallee@gmail.com>
2017-09-10 01:13:59 +0000

    add mock database

---
+++ app.js
@@ -0,0 +1,32 @@
+/* app.js */
+
+//require and instatiate express
+var app = require('express')()
+
+//fake posts to simulate a database
+var posts = [
+  {
+    id: 1,
+    author: 'John',
+    title: 'Templating with EJS',
+    body: 'Blog post number 1'
+  },
+  {
+    id: 2,
+    author: 'Drake',
+    title: 'Express: Starting from the Bottom',
+    body: 'Blog post number 2'
+  },
+  {
+    id: 3,
+    author: 'Emma',
+    title: 'Streams',
+    body: 'Blog post number 3'
+  },
+  {
+    id: 4,
+    author: 'Cody',
+    title: 'Events',
+    body: 'Blog post number 4'
+  }
+]
